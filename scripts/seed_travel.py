4aa82b071ebf2219906d568adc9525a7226b94cd
Justin Maier <Zipp425@gmail.com>
2022-09-21 16:24:51 -0600

    Add FPS option

--- scripts/seed_travel.py
+++ scripts/seed_travel.py
@@ -16,10 +16,17 @@ class Script(scripts.Script):
     def ui(self, is_img2img):
         unsinify = gr.Checkbox(label='Reduce effect of sin() during interpolation', value=True)
         dest_seed = gr.Textbox(label="Destination seed(s) (Comma separated)", lines=1)
-        steps = gr.Number(label="Steps", value=10)
+        steps = gr.Number(label="Travel Steps", value=30)
         save_video = gr.Checkbox(label='Save results as video', value=True)
+        fps = gr.Number(label="Video FPS", value=30)
 
-        return [dest_seed, steps, unsinify, save_video]
+        # Hide video options if not saving video
+        video_options = [fps]
+        def change_visibility(show):
+            return {comp:{"visible": show, "__type__": "update"} for comp in video_options}
+        save_video.change(change_visibility, show_progress=False, inputs=[save_video], outputs=video_options)
+
+        return [dest_seed, steps, fps, unsinify, save_video]
 
     def get_next_sequence_number(path):
         from pathlib import Path
@@ -39,7 +46,7 @@ class Script(scripts.Script):
                 pass
         return result + 1
 
-    def run(self, p, dest_seed, steps, unsinify, save_video):
+    def run(self, p, dest_seed, steps, fps, unsinify, save_video):
         initial_info = None
         images = []
 
@@ -77,7 +84,7 @@ class Script(scripts.Script):
         if save_video:
             import moviepy.video.io.ImageSequenceClip as ImageSequenceClip
             import numpy as np
-            clip = ImageSequenceClip.ImageSequenceClip([np.asarray(i) for i in images], fps=30)
+            clip = ImageSequenceClip.ImageSequenceClip([np.asarray(i) for i in images], fps=fps)
             clip.write_videofile(os.path.join(travel_path, f"travel-{travel_number:05}.mp4"), verbose=False, logger=None)
 
         processed = Processed(p, images, p.seed, initial_info)
